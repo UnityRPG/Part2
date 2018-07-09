d88c027039eebae5a74d1ef9b26cea65b808bffb
Ben Tristem <github@bentristem.com>
2018-07-09 22:06:46 +0100

    Created voice object

---
+++ Assets/Dialogue & Quests/Conversation.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = ("RPG/Conversation"))]
+public class Conversation : ScriptableObject
+{
+    [TextArea]
+    [SerializeField] string openingGambit;
+
+    // TODO consdier creating cast from convo to string
+    public string getConvoAsString()
+    {
+        return openingGambit;
+    }
+}
---
+++ Assets/Dialogue & Quests/Voice.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Voice : MonoBehaviour
+{
+    // configuration parameters, consider SO
+    [SerializeField] Conversation conversation;
+
+    // private instance variables for state
+
+    // cached references for readability
+
+    // messages, then public methods, then private methods...
+    private void Start()
+    {
+        print(conversation.getConvoAsString());
+    }
+}
