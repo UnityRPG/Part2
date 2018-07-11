c61546e17b14bfb80235c9c31881a07db554230c
Ben Tristem <github@bentristem.com>
2018-07-11 13:40:11 +0100

    Add 2nd quest

--- Assets/Dialogue & Quests/Quest.cs
+++ Assets/Dialogue & Quests/Quest.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 // common data and behaviour for all quests
+// TODO consider persistence across scene loads
 public class Quest : MonoBehaviour
 {
     [SerializeField] string questName;
@@ -10,6 +11,7 @@ public class Quest : MonoBehaviour
     public string GetQuestAsString()
     {
         return questName;
+        // TODO consider using the gameObject name
     }
 	
 }
