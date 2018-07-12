b0abe68bfe3a255c1f85818e50457e91f38f66e8
Ben Tristem <github@bentristem.com>
2018-07-12 15:19:06 +0100

    Re-wired and NPC and checked system

--- Assets/Dialogue & Quests/QuestCollision.cs
+++ Assets/Dialogue & Quests/QuestCollision.cs
@@ -2,6 +2,9 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
+// Quest completion criteria, idea is to compose more complex criteria
+// Example other criteria: Kill, Delivery, Gather, Escort. Guess/Solve, Combo. 
 public class QuestCollision : MonoBehaviour // Not from quest
 {
     [SerializeField] Quest questToComplete;
