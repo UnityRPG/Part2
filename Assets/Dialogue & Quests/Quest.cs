3a09077a94f6274b91c796f7adf6969ef181532b
Ben Tristem <github@bentristem.com>
2018-07-12 14:16:10 +0100

    Try a QuestState enum
    Locked
    Unlocked
    In Progress
    Complete

--- Assets/Dialogue & Quests/Quest.cs
+++ Assets/Dialogue & Quests/Quest.cs
@@ -6,12 +6,19 @@ using UnityEngine;
 // TODO consider persistence across scene loads
 public class Quest : MonoBehaviour
 {
-    [SerializeField] string questName;
+    enum QuestState { 
+        Locked,  // some pre-requisite not met
+        Unlocked,  // avilable to be issued
+        Active,
+        Complete
+    }
+
+    [SerializeField] string questName;  // TODO use gameobject name?
+    [SerializeField] QuestState questState;
 
     public string GetQuestAsString()
     {
         return questName;
-        // TODO consider using the gameObject name
     }
 	
 }
