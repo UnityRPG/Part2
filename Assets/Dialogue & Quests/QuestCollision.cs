2515a799a31b3dcfbe86039f028d51aa9f6a7890
Ben Tristem <github@bentristem.com>
2018-07-16 14:24:26 +0100

    Deciding on order to tackle quest types

--- Assets/Dialogue & Quests/QuestCollision.cs
+++ Assets/Dialogue & Quests/QuestCollision.cs
@@ -2,10 +2,6 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-
-// Quest completion criteria, idea is to compose more complex criteria
-// Example other criteria: Delivery, Gather, Escort. Guess/Solve, Combo
-
 namespace RPG.Quests
 {
     public class QuestCollision : QuestionCompletion // Not from quest
--- Assets/Dialogue & Quests/QuestionCompletion.cs
+++ Assets/Dialogue & Quests/QuestionCompletion.cs
@@ -4,6 +4,14 @@ using UnityEngine;
 
 namespace RPG.Quests
 {
+    // Quest completion criteria, idea is to compose more complex criteria
+    // Example other quest criteria TODO
+    // Escort
+    // Gather (just before inventory)
+    // Delivery (after inventory)
+    // Combo if required @Rick?
+    // Guess / Solve if required @Rick?
+
     public class QuestionCompletion : MonoBehaviour
     {
 
