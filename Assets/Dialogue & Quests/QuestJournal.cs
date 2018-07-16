2fcf70a8b98a50a4dc5b04c455aec01198ac8aa5
Ben Tristem <github@bentristem.com>
2018-07-16 13:27:42 +0100

    Drive Quest UI from game objects

--- Assets/Dialogue & Quests/QuestJournal.cs
+++ Assets/Dialogue & Quests/QuestJournal.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
@@ -10,8 +11,14 @@ public class QuestJournal : MonoBehaviour  // todo really needs to be?
     // private instance variables for state
 
     // cached references for readability
+    [SerializeField] Transform questParent;
 
     // messages, then public methods, then private methods...
+    private void Update()
+    {
+        UpdateQuestsFromScene(); // TODO on Update really?
+    }
+
     public void AddQuest(Quest quest)
     {
         // TODO get actual quest description
@@ -24,5 +31,23 @@ public class QuestJournal : MonoBehaviour  // todo really needs to be?
         print("completing " + quest);
         GetComponent<Text>().text = "";
         quest.SetQuestState(QuestState.Complete);
+    }
+
+    private void UpdateQuestsFromScene()
+    {
+        GetComponent<Text>().text = "";
+        foreach (Transform child in questParent)
+        {
+            ListQuestIfStarted(child);
+        }
+    }
+
+    private void ListQuestIfStarted(Transform child)
+    {
+        var questState = child.gameObject.GetComponent<Quest>().GetQuestState();
+        if (questState == QuestState.Started)
+        {
+            GetComponent<Text>().text += child.gameObject.name + '\n';
+        }
     }
 }
