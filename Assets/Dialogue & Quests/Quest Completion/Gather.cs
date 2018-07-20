cc31c2c0b012b1b073ce345d91089a1c453f67ba
Ben Tristem <github@bentristem.com>
2018-07-20 15:39:06 +0100

    Upgrade Unity version & started Gather

---
+++ Assets/Dialogue & Quests/Quest Completion/Gather.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RPG.Quests
+{
+    public class Gather : QuestCompletion
+    {
+        void OnCollisionEnter() // 1st copy from Collision.cs
+        {
+            if (questToComplete.QuestState == QuestState.Started)
+            {   
+                Destroy(gameObject);
+                // TODO put object in inventory
+                CompleteQuest();
+            }
+        }
+    }
+}
--- Assets/Dialogue & Quests/Quest Completion/Trigger.cs
+++ Assets/Dialogue & Quests/Quest Completion/Trigger.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 namespace RPG.Quests
 {
-    public class Collision : QuestCompletion // Not from quest
+    public class Trigger : QuestCompletion // Not from quest
     {
         private void OnTriggerEnter(Collider other)
         {
