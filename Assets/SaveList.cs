d46baaa1d9669583dd904776b635e814bacce70a
Samuel Pattuzzi <sam@pattuzzi.it>
2018-08-19 13:01:24 +0100

    Spawn save items for saves.

---
+++ Assets/SaveList.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using RPG.Core.Saving;
+using UnityEngine.UI;
+
+public class SaveList : MonoBehaviour {
+
+    [SerializeField]
+    GameObject SaveItemPrefab;
+    
+    void OnEnable()
+    {
+        var SaveLoad = FindObjectOfType<SaveLoad>();
+        foreach (GameObject item in GetComponent<Transform>())
+        {
+            Destroy(item);
+        }
+        foreach (var save in SaveLoad.GetSaveFileList())
+        {
+            var item = Instantiate(SaveItemPrefab, GetComponent<Transform>());
+            var textComponent = item.GetComponentInChildren<Text>();
+            textComponent.text = save;
+        }
+    }
+
+}
