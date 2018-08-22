ee3814b897814e5a5fd859d15b3f0c396162a538
Samuel Pattuzzi <sam@pattuzzi.it>
2018-08-22 17:08:30 +0100

    Basic drawing to an editor.

---
+++ Assets/Editor/DialogueEditor.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+
+public class DialogueEditor : EditorWindow {
+
+    Vector2 scrollPosition = Vector2.zero;
+    Rect Canvas = Rect.zero;
+
+    [MenuItem("Window/Dialogue Editor")]
+    public static void ShowWindow()
+    {
+        GetWindow(typeof(DialogueEditor), false, "Dialogue Editor");
+    }
+
+    void OnGUI()
+    {
+        Canvas.size = new Vector2(4000, 4000);
+        scrollPosition = GUI.BeginScrollView(new Rect(0, 0, position.x, position.y), scrollPosition, Canvas);
+        GUI.Box(Canvas, "Hi");
+        GUI.EndScrollView();
+    }
+}
