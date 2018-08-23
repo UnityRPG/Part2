e1bd7d439579e475702e037fe7bc73fb960266cb
Samuel Pattuzzi <sam@pattuzzi.it>
2018-08-23 14:51:22 +0100

    Dialogue editor draws nodes.
    Included a script from "http://wiki.unity3d.com/index.php/Show_Built_In_Resources" to show all resources available in the editor.

--- Assets/Editor/DialogueEditor.cs
+++ Assets/Editor/DialogueEditor.cs
@@ -3,22 +3,38 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
 
-
-public class DialogueEditor : EditorWindow {
-
-    Vector2 scrollPosition = Vector2.zero;
-    Rect Canvas = new Rect(0,0, 4000, 4000);
-
-    [MenuItem("Window/Dialogue Editor")]
-    public static void ShowWindow()
+namespace RPG.Editor.Dialogue
+{
+    public class DialogueEditor : EditorWindow
     {
-        GetWindow(typeof(DialogueEditor), false, "Dialogue Editor");
-    }
 
-    void OnGUI()
-    {
-        scrollPosition = GUI.BeginScrollView(new Rect(Vector2.zero, position.size), scrollPosition, Canvas);
+        Vector2 scrollPosition = Vector2.zero;
+        Rect Canvas = new Rect(0, 0, 4000, 4000);
+        List<Node> nodes = new List<Node>();
 
-        GUI.EndScrollView();
+        [MenuItem("Window/Dialogue Editor")]
+        public static void ShowWindow()
+        {
+            GetWindow(typeof(DialogueEditor), false, "Dialogue Editor");
+        }
+
+        void OnEnable()
+        {
+            nodes.Clear();
+            nodes.Add(new Node(new Vector2(100, 200)));
+            nodes.Add(new Node(new Vector2(400, 200)));
+        }
+
+        void OnGUI()
+        {
+            scrollPosition = GUI.BeginScrollView(new Rect(Vector2.zero, position.size), scrollPosition, Canvas);
+
+            foreach (var node in nodes)
+            {
+                node.Draw();
+            }
+    
+            GUI.EndScrollView();
+        }
     }
 }
---
+++ Assets/Editor/Node.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+namespace RPG.Editor.Dialogue
+{
+    public class Node
+    {
+        private GUIStyle style = new GUIStyle();
+        private string text;
+        private Rect rect = new Rect();
+
+        public Node(Vector2 position)
+        {
+            style.normal.background = EditorGUIUtility.Load("node0") as Texture2D;
+            style.border = new RectOffset(12, 12, 12, 12);
+            style.padding = new RectOffset(20, 20, 20, 20);
+            rect.size = new Vector2(200, 200);
+            rect.position = position;
+        }
+
+        public void Draw()
+        {
+            GUILayout.BeginArea(rect, style);
+            text = GUILayout.TextArea(text);
+            GUILayout.EndArea();
+        }
+
+    }
+}
