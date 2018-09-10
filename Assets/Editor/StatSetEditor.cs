ff7eb29df4e94e1dad412c5ef721174902e497fb
Samuel Pattuzzi <sam@pattuzzi.it>
2018-09-10 11:34:35 +0100

    CSV importer and parser class with editor.

---
+++ Assets/Editor/StatSetEditor.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+namespace RPG.Core.Stats
+{
+    [CustomEditor(typeof(StatSet))]
+    public class StatSetEditor : UnityEditor.Editor
+    {
+        public override void OnInspectorGUI()
+        {
+            if (GUILayout.Button("Import CSV"))
+            {
+                string path = EditorUtility.OpenFilePanel("Import CSV", "", "csv");
+                var importer = new CSVImporter(path);
+                importer.Load();
+                Debug.Log(importer.GetCell("1", 10));
+            }
+
+            DrawDefaultInspector();
+        }
+    }
+}
---
+++ Assets/_Core/CSVImporter.cs
@@ -0,0 +1,70 @@
+﻿using System.IO;
+
+namespace RPG.Core
+{
+    public class CSVImporter
+    {
+        string path;
+        char[] seperators;
+        string[] headers;
+        string[][] cells;
+
+        public CSVImporter(string path, char[] seperators = null)
+        {
+            this.path = path;
+            if (seperators == null)
+            {
+                seperators = new char[] { ',' };
+            }
+            this.seperators = seperators;
+        }
+
+        public void Load()
+        {
+            string[] lines = File.ReadAllLines(path);
+            headers = null;
+            cells = new string[lines.Length - 1][];
+            for (int i = 0; i < lines.Length; i++)
+            {
+                var line = lines[i];
+                string[] lineItems = line.Split(seperators);
+                if (i == 0)
+                {
+                    headers = lineItems;
+                    continue;
+                }
+
+                cells[i - 1] = lineItems;
+            }
+        }
+
+        public string GetCell(string header, int row)
+        {
+            int column = GetIndexForHeader(header);
+            if (column == -1)
+            {
+                return null;
+            }
+
+            return cells[row][column];
+        }
+
+        public int GetLength()
+        {
+            return cells.Length;
+        }
+
+        public int GetIndexForHeader(string header)
+        {
+            for (int i = 0; i < headers.Length; i++)
+            {
+                if (header == headers[i])
+                {
+                    return i;
+                }
+            }
+
+            return -1;
+        }
+    }
+}
