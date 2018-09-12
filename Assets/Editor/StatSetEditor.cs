5a76a462697aa203216a7dcfb7970de94129037e
Samuel Pattuzzi <sam@pattuzzi.it>
2018-09-12 11:34:39 +0100

    Import enemies too.

--- Assets/Editor/StatSetEditor.cs
+++ Assets/Editor/StatSetEditor.cs
@@ -28,15 +28,39 @@ namespace RPG.Core.Stats
             currentImporter = new CSVImporter(path);
             currentImporter.Load();
 
+            ImportPlayer();
+            ImportEnemies();
+
+        }
+
+        private void ImportPlayer()
+        {
             var property = serializedObject.FindProperty("playerLevels");
 
             ImportProperty(property, "Base Health", "Player", "health");
             ImportProperty(property, "Base Damage (per hit)", "Player", "damagePerHit");
             ImportProperty(property, "Hit speed / DPS", "Player", "hitsPerSecond");
             ImportProperty(property, "Typical Ability Damage", "Player", "specialDamage");
-            
+
             ImportProperty(property, "XP to level up", "Player", "XPToLevelUp");
             ImportProperty(property, "Experience per enemy kill", "Player", "XPPerEnemyKill");
+        }
+
+        private void ImportEnemies()
+        {
+            var property = serializedObject.FindProperty("enemyClasses");
+
+            for (int i = 0; i < property.arraySize; i++)
+            {
+                var enemyClassProperty = property.GetArrayElementAtIndex(i);
+                var enemyClassName = enemyClassProperty.FindPropertyRelative("className").stringValue;
+                var levelsProperty = enemyClassProperty.FindPropertyRelative("enemyLevels");
+
+                ImportProperty(levelsProperty, "Health", enemyClassName, "health");
+                ImportProperty(levelsProperty, "Damage per hit", enemyClassName, "damagePerHit");
+                ImportProperty(levelsProperty, "Hit speed / DPS", enemyClassName, "hitsPerSecond");
+                ImportProperty(levelsProperty, "Big Hit / special", enemyClassName, "specialDamage");
+            }
 
         }
 
