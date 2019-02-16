d3c5aa75ad4ef6238a76f6cfb9704ca6a6878b7d
Samuel Pattuzzi <sam@pattuzzi.it>
2019-02-16 17:23:52 +0000

    Roll CameraRaycaster into PlayerControl

--- Assets/Scripts/Control/PlayerControl.cs
+++ Assets/Scripts/Control/PlayerControl.cs
@@ -4,39 +4,116 @@ using RPG.Core;
 using RPG.Combat;
 using RPG.Movement;
 using RPG.SpecialActions;
+using RPG.Dialogue;
+using RPG.Inventories;
+using UnityEngine.AI;
+using System;
+using UnityEngine.EventSystems;
 
 namespace RPG.Control
 {
     public class PlayerControl : MonoBehaviour
     {
+        [SerializeField] Texture2D defaultCursor = null;
+        [SerializeField] Texture2D walkCursor = null;
+        [SerializeField] Texture2D enemyCursor = null;
+        [SerializeField] Texture2D pickupCursor = null;
+        [SerializeField] Texture2D talkCursor = null;
+        [SerializeField] Vector2 cursorHotspot = new Vector2(0, 0);
+
+        const int POTENTIALLY_WALKABLE_LAYER = 8;
+        const float maxRaycastDepth = 100f; // Hard coded value
+
         Mover mover;
         SpecialAbilities abilities;
         WeaponSystem weaponSystem;
 
-        int desiredSpecialAbility = -1;
-        bool wantsToMove = false;
-        Vector3 desiredLocation;
-        GameObject target;
+        Texture2D requestedCursor;
 
         void Start()
         {
             mover = GetComponent<Mover>();
             abilities = GetComponent<SpecialAbilities>();
             weaponSystem = GetComponent<WeaponSystem>();
-            
-            RegisterForMouseEvents();
-        }
-
-        private void RegisterForMouseEvents()
-        {
-            var cameraRaycaster = FindObjectOfType<CameraRaycaster>();
-            cameraRaycaster.onMouseOverEnemy += OnMouseOverEnemy;
-            cameraRaycaster.onMouseOverPotentiallyWalkable += OnMouseOverPotentiallyWalkable;
         }
 
         void Update()
         {
             ScanForAbilityKeyDown();
+
+            requestedCursor = defaultCursor;
+
+            PerformRaycasts();
+            Cursor.SetCursor(requestedCursor, cursorHotspot, CursorMode.Auto);
+        }
+
+        private void PerformRaycasts()
+        {
+            if (EventSystem.current.IsPointerOverGameObject()) return;
+
+            var currentScreenRect = new Rect(0, 0, Screen.width, Screen.height);
+            if (!currentScreenRect.Contains(Input.mousePosition)) return;
+
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            // Specify layer priorities below, order matters
+            ConversationSource conversation = RaycastForComponent<ConversationSource>(ray, talkCursor);
+            if (conversation != null) 
+            {
+                if (Input.GetMouseButtonDown(0))
+                {
+                    conversation.VoiceClicked();
+                }
+            }
+            
+            EnemyAI enemy = RaycastForComponent<EnemyAI>(ray, enemyCursor);
+            if (enemy != null) 
+            {
+                if (Input.GetMouseButtonDown(0))
+                {
+                    Attack(enemy.gameObject);
+                }
+                if (Input.GetMouseButtonDown(1))
+                {
+                    QuickSpecialAbility(enemy.gameObject);
+                }
+                return;
+            }
+
+            Pickup pickup = RaycastForComponent<Pickup>(ray, pickupCursor);
+            if (pickup != null) {
+                if (Input.GetMouseButtonDown(0))
+                {
+                    pickup.PickupItem();
+                }
+                return;
+            }
+
+            var location = NavigateToWalkable(ray);
+            if (location.HasValue) {
+                if (Input.GetMouseButton(0))
+                {
+                    Walk(location.Value);
+                }
+                return;    
+            }
+        }
+
+        void Walk(Vector3 destination)
+        {
+            CancelAll();
+            mover.SetDestination(destination);
+        }
+
+        void Attack(GameObject target)
+        {
+            CancelAll();
+            weaponSystem.AttackTarget(target);
+        }
+
+        void QuickSpecialAbility(GameObject target)
+        {
+            CancelAll();
+            abilities.RequestSpecialAbility(0, target);
         }
 
         void ScanForAbilityKeyDown()
@@ -51,34 +128,47 @@ namespace RPG.Control
             }
         }
 
-        void OnMouseOverPotentiallyWalkable(Vector3 destination)
-        {
-            if (Input.GetMouseButton(0))
-            {
-                CancelAll();
-                mover.SetDestination(destination);
-            }
-        }
-
-        void OnMouseOverEnemy(EnemyAI enemy)
-        {
-            if (Input.GetMouseButton(0))
-            {
-                CancelAll();
-                weaponSystem.AttackTarget(enemy.gameObject);
-            }
-            else if (Input.GetMouseButtonDown(1))
-            {
-                CancelAll();
-                abilities.RequestSpecialAbility(0, enemy.gameObject);
-            }
-        }
-
         void CancelAll()
         {
             weaponSystem.StopAttacking();
             abilities.CancelRequest();
             mover.ClearDestination();
+        }
+
+        private C RaycastForComponent<C>(Ray ray, Texture2D cursor) where C : Component
+        {
+            GameObject gameObjectHit = GetFirstHitGameObject(ray);
+            var hit = gameObjectHit.GetComponent<C>();
+            if (hit != null)
+            {
+                requestedCursor = cursor;
+                return hit;
+            }
+            return null;
+        }
+
+        private Nullable<Vector3> NavigateToWalkable(Ray ray)
+        {
+            LayerMask potentiallyWalkableLayer = 1 << POTENTIALLY_WALKABLE_LAYER;
+            var raycastHits = Physics.RaycastAll(ray, maxRaycastDepth, potentiallyWalkableLayer);
+            foreach (var raycasthit in raycastHits)
+            {
+                NavMeshHit navMeshHit;
+                if (NavMesh.SamplePosition(raycasthit.point, out navMeshHit, 0.2f, NavMesh.AllAreas))
+                {
+                    requestedCursor = walkCursor;
+                    return navMeshHit.position;
+                }
+            }
+            return null;
+        }
+
+        private GameObject GetFirstHitGameObject(Ray ray)
+        {
+            RaycastHit hitInfo;
+            Physics.Raycast(ray, out hitInfo, maxRaycastDepth);
+            var gameObjectHit = hitInfo.collider.gameObject;
+            return gameObjectHit;
         }
     }
 }
